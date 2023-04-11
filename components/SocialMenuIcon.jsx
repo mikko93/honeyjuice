245c2d32a780827b4cb8d87244f4b042e3eca598
mikko93 <mikkoeeromiettinen@gmail.com>
2023-04-11 23:48:05 +0300

    Fixed aria-label attr reference

--- components/SocialMenuIcon.jsx
+++ components/SocialMenuIcon.jsx
@@ -3,11 +3,11 @@ import styles from '@styles/SocialMenu.module.scss';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css'; // optional
 
-const SocialMenuIcon = ({ link, icon, tooltip }) => {
+const SocialMenuIcon = ({ link, icon, tooltip, arialabel }) => {
   return (
     <Tippy content={<span>{tooltip}</span>}>
       <div className={styles.social_iconContainer}>
-        <a href={link} aria-label={aria-label} target='_blank' rel='noreferrer nofollow'>
+        <a href={link} aria-label={arialabel} target='_blank' rel='noreferrer nofollow'>
           {icon}
         </a>
       </div>
