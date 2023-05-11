5f8c04dd73153ce755782bffcbc8e306d20a80ad
mikko93 <mikkoeeromiettinen@gmail.com>
2023-05-11 21:56:35 +0300

    Revert "Color changes"
    This reverts commit fbdebe09f605b74db1f2917127f0a21fa4547160.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styles from '@styles/Home.module.scss';
 import Image from 'next/image'
 import Link from 'next/link';
 import logo from '../public/static/logo.png'
-import groupPhoto from '../public/static/group-photo.jpg'
+ import groupPhoto from '../public/static/group-photo.jpg'
 
 export default function Home() {
   return (
@@ -20,7 +20,6 @@ export default function Home() {
       Combining the Finnish spirit of love, life and joy with the German sense of humor and the ambitious Spanish work ethic 
       and promoting the freedom of self-expression that lyricist Asalya is used to from her home country Uzbekistan, this quintet of wannabe high school dropouts is bound to be the next big thing.
       </p>
-      <section className={styles.photo_group}>
       { <Image
         src={groupPhoto}
         alt='group photo'
@@ -28,7 +27,6 @@ export default function Home() {
         height='auto'
         style={{objectFit:"contain"}}
       /> }
-      </section>
       <figcaption className={styles.photo_caption}>
         Asalya (vocals), Philip (guitar), Mikko (bass), Albert (drums) and Lauri (guitar)
       </figcaption>
