ffb99f35cfc0f43c3ae2954557c9883e11a3731b
mikko93 <mikkoeeromiettinen@gmail.com>
2024-05-10 00:22:29 +0300

    Update urls now that first single is out

--- components/SocialMenu.jsx
+++ components/SocialMenu.jsx
@@ -17,19 +17,19 @@ const SocialMenu = () => {
     <div className={styles.social}>
       <SocialMenuIcon
         icon={<SiApplemusic />}
-        link='https://music.apple.com/us/artist/honey-juice/1743831141'
+        link='https://music.apple.com/artist/honey-juice/1743831141'
         tooltip='Apple Music'
         arialabel='Apple Music'
       />
       <SocialMenuIcon
         icon={<SiFacebook />}
-        link='https://www.facebook.com/honeyjuiceband'
+        link='https://facebook.com/honeyjuiceband'
         tooltip='Facebook'
         arialabel='Facebook'
       />
       <SocialMenuIcon
         icon={<SiInstagram />}
-        link='https://www.instagram.com/honeyjuiceband/'
+        link='https://instagram.com/honeyjuiceband/'
         tooltip='Instagram'
         arialabel='Instagram'
       />
@@ -40,22 +40,22 @@ const SocialMenu = () => {
         arialabel='Spotify'
         rel='preconnect'
       />
-      {/*
+
       <SocialMenuIcon
         icon={<SiTidal />}
-        link='https://www.tidal.com'
+        link='https://tidal.com/browse/artist/47463382'
         tooltip='Tidal'
         arialabel='Tidal'
-  />*/}
+      />
       <SocialMenuIcon
         icon={<SiTiktok />}
-        link='https://www.tiktok.com/@honeyjuiceband'
+        link='https://tiktok.com/@honeyjuiceband'
         tooltip='TikTok'
         arialabel='TikTok'
       />
       <SocialMenuIcon
         icon={<SiYoutube />}
-        link='https://www.youtube.com/@HoneyJuice-band'
+        link='https://music.youtube.com/channel/UCn2KWdxXXWuItTjrakN1ibA'
         tooltip='YouTube'
         arialabel='YouTube'
       />
